7b3f2ab091b0778b1be4e37e43a21bee2afea0a1
aviralm52 <aviralm52@gmail.com>
2025-05-13 18:15:50 +0530

    product page

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -8,6 +8,23 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { sortValues } from "../search-params";
 
 export const productsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const product = await ctx.db.findByID({
+        collection: "products",
+        id: input.id,
+      });
+      return {
+        ...product,
+        image: product.image as Media | null,
+        tenant: product.tenant as Tenant & { image: Media | null },
+      };
+    }),
   getMany: baseProcedure
     .input(
       z.object({
